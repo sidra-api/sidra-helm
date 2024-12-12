a88758cb2ca1bb7c413b1c5b08e5b3193ab32e0b
lukluk santoso <lukluk.santoso@bankmas.co.id>
2024-12-12 09:40:38 +0800

    update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
-
-# GitHub organization and personal access token
-ORG="sidra-api"
-TOKEN=$TOKEN
-# Configure git to use the personal access token for authentication
-
-
-# Get list of repositories in the organization
-repos=$(curl -s -H "Authorization: token $TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/orgs/$ORG/repos | jq -r '.[].ssh_url')
-
-rm -Rf ./plugins || true
-rm -Rf ./services || true
-mkdir -p ./plugins || true
-mkdir -p ./services || true
-
-# Build the plugins
-for repo in $repos; do
-    repo_name=$(basename -s .git $repo)
-    if [[ $repo_name == plugin-* ]]; then        
-        echo "Cloning $repo_name"
-        git clone "$repo" "./plugins/$repo_name"
-        # Build plugin binary
-        # cd "./plugins/$repo_name" || exit
-        # go build -o main
-        # cd - || exit
-    fi    
-done
+rm -rf ./services/*
+rm -rf ./plugins/*
+# Array of repository URLs
+repos=(
+    "https://github.com/sidra-api/plugin-basic-auth.git"
+    "https://github.com/sidra-api/plugin-jwt.git"
+    "https://github.com/sidra-api/plugin-rate-limit.git"
+    "https://github.com/sidra-api/plugin-whitelist.git"
+    "https://github.com/sidra-api/plugin-cache.git"
+    "https://github.com/sidra-api/plugin-rsa.git"
+)
 
 # Clone sidra-config and sidra-plugins-hub
 git clone https://github.com/sidra-api/sidra-data-plane.git ./services/sidra-data-plane
-git clone git@github.com:sidra-api/sidra-plugins-hub.git ./services/sidra-plugins-hub
+git clone https://github.com/sidra-api/sidra-plugins-hub.git ./services/sidra-plugins-hub
+
+# Clone additional repositories
+# Clone additional repositories
+for repo in "${repos[@]}"; do
+    repo_name=$(basename "$repo" .git)
+    git clone "$repo" "./plugins/$repo_name"
+done
